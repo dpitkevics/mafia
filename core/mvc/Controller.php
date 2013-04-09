e6a726b5b63cca38bcd094fc50a611fa8e0422d2
Daniels Pitkevics <daniels.pitkevics@gmail.com>
2013-04-09 12:32:11 +0300

    Bugfix in drawing

--- core/mvc/Controller.php
+++ core/mvc/Controller.php
@@ -171,7 +171,7 @@ class Controller {
                 $path = 'views' . $path;
             $path = ROOT_DIR . $path . '.php';
         }
-        
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
         extract ($params);
         ob_start();
         include $path;
